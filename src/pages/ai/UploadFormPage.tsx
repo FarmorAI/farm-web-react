99d2ba7d79c892fb45b36d3b65128840042f8245
hogi2020 <junmyung2020@gmail.com>
2025-03-09 00:33:52 +0900

    upload form update

--- src/pages/ai/UploadFormPage.tsx
+++ src/pages/ai/UploadFormPage.tsx
@@ -5,8 +5,11 @@ import FileUploader from "../../components/ai/uploadform/FileUploader";
 import ProcessingSteps from "../../components/ai/uploadform/Step";
 import RatingSection from "../../components/ai/uploadform/RatingSection";
 
+interface AiResultValue { [key: string]: number }
+
 const AIUploadPage: React.FC = () => {
   const [processedImage, setProcessedImage] = useState<string | null>(null);
+  const [respData, setRespData] = useState<AiResultValue>({'': 0});
 
   const handleUpload = async (files: File[]) => {
     const formData = new FormData();
@@ -17,6 +20,8 @@ const AIUploadPage: React.FC = () => {
         "http://localhost:6060/api/upload",
         formData
       );
+      console.log(response.data.quality);
+      setRespData(response.data.quality);
       setProcessedImage(response.data);
     } catch (error) {
       console.error("파일 업로드 오류:", error);
@@ -36,11 +41,17 @@ const AIUploadPage: React.FC = () => {
         <FileUploader onUpload={handleUpload} onViewResults={handleViewResults} />
         
         {processedImage && (
-          <img
-            src={processedImage}
-            alt="AI 분석 결과"
-            className="mt-6 border rounded"
-          />
+          <>
+            <img src={processedImage} alt="AI 분석 결과" className="mt-6 border rounded" />
+            <div className="grid grid-cols-3 gap-3">
+              {Object.entries(respData).map(([key, value]) => (
+                <div key={key} className="flex justify-between bg-gray-200 p-2 rounded-md">
+                  <span className="text-gray-600">{key}:</span>
+                  <span className="text-red-600 font-medium"> {value*100}%</span>
+                </div>
+              ))}
+            </div>
+          </>
         )}
         <br/>
         <ProcessingSteps />
