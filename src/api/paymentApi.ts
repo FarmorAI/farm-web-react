c0ff892f1b2609fc66468d4e521e66816403ebfc
hogi2020 <junmyung2020@gmail.com>
2025-03-25 14:33:37 +0900

    update file

--- src/api/paymentApi.ts
+++ src/api/paymentApi.ts
@@ -4,22 +4,36 @@ import { getCookie } from "../util/cookieUtill";
 
 
 export const handlePaymentNaver = async(selectedPlan: string, selectedPrice: number): Promise<void> => {
-   const response = await axios.post(`${API_BASE_URL}/payment/naverpay`, {
-      subsPlan: selectedPlan,
-      subsPrice: selectedPrice
-   }, {
-      headers: { 
-         "Content-Type": "application/json", 
-         "Authorization": `Bearer ${getCookie("jwt")}`
+   try {
+      const response = await axios.post(`${API_BASE_URL}/payment/naverpay`, {
+         subsPlan: selectedPlan,
+         subsPrice: selectedPrice
+      }, {
+         headers: { 
+            "Content-Type": "application/json", 
+            "Authorization": `Bearer ${getCookie("jwt")}`
+         }
+      })
+      const result = await response.data
+      window.location.href = `https://test-m.pay.naver.com/payments/${result.body.reserveId}`
+   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   } catch(error: any) {
+      console.log(error)
+      if (error.response && error.response.status === 401) {
+         alert("로그인이 필요합니다.")
       }
-   })
-   const result = await response.data
-   window.location.href = `https://test-m.pay.naver.com/payments/${result.body.reserveId}`
+   }
 }
 
 export const handlePaymentCancel = async(token: string) => {
    try {
-      const response = await axios.post(`${API_BASE_URL}/payment/cancel`, { token });
+      const response = await axios.post(`${API_BASE_URL}/payment/cancel`, { 
+         token 
+      }, {
+         headers: { 
+            "Authorization": `Bearer ${getCookie("jwt")}`
+         }
+      });
       alert(response.data.result + " 결제 취소 완료!");
    } catch (error) {
       console.log(error);
